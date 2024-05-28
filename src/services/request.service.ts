1c923e0dc6a01f2f8636e2da3716f49caf0abbe4
albams5 <albams51289@gmail.com>
2024-05-28 08:43:45 +0200

    refactor: getMovies

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -113,7 +113,7 @@ export const getMovies = async () => {
     const response = await dataMovies.json();
     const movies = response.data;
     const movieCards = await Promise.all(
-      movies.map((movie: Movie) => genreFetch(movie))
+      movies.length > 0 && movies?.map((movie: Movie) => genreFetch(movie))
     );
     return movieCards;
   }catch(error){
